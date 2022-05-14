93110488154d43ea223825463c0e458acf864342
pistosmin <tajiklee@naver.com>
2022-05-14 21:31:37 +0900

    Chapter 1 done

---
+++ src/components/search_bar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+// const Component = React.Component;
+
+// const SearchBar = () => {
+//     return <input />;
+    
+// };
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: 'Starting Value' };
+    }
+
+
+    render() {
+        // this.state.term = event.target.value; //BAD 이렇게 코딩하지 말것
+        // return <input onChange={event => console.log(event.target.value)}/>
+        
+        return (
+            <div>
+                <input 
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })}
+                />
+                {/* Value of the input: {this.state.term} */}
+            </div>
+
+        )
+    }
+
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+    //     // console.log(event);
+    // }
+
+};
+// new SearchBar;
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+
 // import { Provider } from 'react-redux';
 // import { createStore, applyMiddleware } from 'redux';
 
 // import App from './components/app';
 // import reducers from './reducers';
 
-// const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware()(createStore);'
+
+const API_KEY = 'AIzaSyBB17EWI3M4nH1TIe7wdFkMHLnJJlBUcLE';
+
 
 // ReactDOM.render(
 //   <Provider store={createStoreWithMiddleware(reducers)}>
@@ -17,7 +24,9 @@ import ReactDOM from 'react-dom';
 // Create a new component. This component should produce some HTML
 const App = () => {
   
-  return <div>Hi!</div>;
+  return <div>
+    <SearchBar />
+  </div>;
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)

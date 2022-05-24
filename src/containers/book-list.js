3480c474319aa760e5bfaf8c9fc64baac06e505c
pistosmin <tajiklee@naver.com>
2022-05-24 23:40:48 +0900

    redux day 4

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -5,11 +5,15 @@ import { bindActionCreators } from 'redux';
 
 // export default class BookList extends Component {
 class BookList extends Component {
-
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item"> { book.title }</li>
+                <li 
+                    key={book.title} 
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item"> 
+                    { book.title }
+                </li>
             );
         });
     }
@@ -35,4 +39,4 @@ function mapDispatchToProps(dispatch) {
     return bindActionCreators({ selectBook: selectBook } , dispatch);
 }
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);

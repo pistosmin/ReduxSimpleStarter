cd3ab62093cbed2ebb31e6e8040b7f40cf0b4cfc
pistosmin <tajiklee@naver.com>
2022-06-02 23:47:45 +0900

    api1

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 import SearchBar from '../containers/search_bar';
 
+//openweathermap api key
+//5f9e44aeac42b4f56406fc415816b25e
+
+
 export default class App extends Component {
   render() {
     return (

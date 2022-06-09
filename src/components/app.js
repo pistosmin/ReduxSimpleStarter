5a4f49956585150eb2f663eba463949f2a1c93d0
pistosmin <tajiklee@naver.com>
2022-06-09 23:56:13 +0900

    restart

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,6 @@ import SearchBar from '../containers/search_bar';
 
 //openweathermap api key
 //5f9e44aeac42b4f56406fc415816b25e
-
 export default class App extends Component {
   render() {
     return (
@@ -13,4 +12,4 @@ export default class App extends Component {
       </div>
     );
   }
-}426071984494
+}
